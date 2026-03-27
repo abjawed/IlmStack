8f7b91a941e7877510e19ee952e65e488e79327a
Aquib Jawed <86352184+abjawed@users.noreply.github.com>
2026-03-27 23:33:52 +0530

    Added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  //url: 'https://your-docusaurus-site.example.com',
+  url: 'https://abjawed.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  //baseUrl: '/',
+  baseUrl: '/IlmStack/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'abjawed', // Usually your GitHub org/user name.
+  projectName: 'IlmStack', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
